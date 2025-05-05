c0e8ea6c1a68e90c3a227cf59b00e43d8f539b02
EzequielMelo <ezequielanthonymelo@gmail.com>
2025-05-05 19:38:07 -0300

    cambios en el comportamiento de la conexion entre mi frontend y backend

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -3,30 +3,22 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 
 const app = express();
 const PORT = parseInt(process.env.PORT || "3000", 10);
 
-// configuracion de cors
-app.use(
-  cors({
-    origin: "*", // En produccion especificar el url de mi app
-    credentials: true,
-  }),
-);
+// Permitir solicitudes desde cualquier origen
+app.use(cors({ origin: "*", credentials: true }));
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(
+    `🚀 Server running on port ${PORT} in ${process.env.NODE_ENV || "undefined"} mode`,
+  );
 });
-
-/* For local use
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
-*/
--- frontEnd/src/api/axios.ts
+++ frontEnd/src/api/axios.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
-import Constants from "expo-constants";
+import Constants from "expo-constants"; // Asegúrate de importar Constants
 
+// Si estás usando Expo, puedes obtener la URL desde los extras
+const backendUrl =
+  Constants.expoConfig?.extra?.backendUrl || "http://192.168.1.36:3000/api"; // URL por defecto para desarrollo local
+
+/*
 const backendUrl =
   Constants.expoConfig?.extra?.backendUrl ||
   process.env.EXPO_PUBLIC_API_URL ||
   "https://proyecto-mobileapp.onrender.com/api";
+ */
 
 if (!backendUrl) {
   console.warn("⚠️ No se pudo determinar la URL del backend");
